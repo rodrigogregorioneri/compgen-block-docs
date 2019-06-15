cb548459f3bda63a8100fa1d6b9122006b6049ee
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2019-06-15 20:58:56 -0300

    Conexão com MongoDB, Serviço teste REST e Swagger

---
+++ src/main/java/com/hackaton/compgenblockdocs/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.hackaton.compgenblockdocs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/test")
+public class TestController {
+
+
+    @GetMapping
+    public String getAll(){
+        return "teste";
+    }
+}
