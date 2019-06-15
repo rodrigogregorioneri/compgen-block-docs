077294a1c4fa262fa4a318da52fed63f1d50bd50
Rodrigo Neri <rodrigogregorioneri@hotmail.com>
2019-06-15 19:20:53 -0300

    subindo projeto

---
+++ src/main/java/com/hackaton/compgenblockdocs/CompgenBlockDocsApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.compgenblockdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompgenBlockDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompgenBlockDocsApplication.class, args);
+	}
+
+}
